916914e754a8d66804bcfaa161b87d20637b38fe
Tim Mardis <mardistj96@yahoo.com>
2022-08-28 14:24:29 -0500

    Added user paramater promts

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,10 @@
 var lowerpassword = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f"];
 var upperpassword = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", ", Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F"];
 var numberpassword = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8];
-var specialpassword = [ "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!","$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")"];
-var passwordlength = 10
-var lowercase = true
-var uppercase = true
-var numbers = true
-var specialchar = true
+var specialpassword = ["!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!","$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")"];
+var notrunyet = 1
+const element = document.querySelector('.btn');
+element.style.backgroundColor = 'red';
 
 function paramaters() {
   var promptlength = prompt("Please enter how long you want your password to be.");
@@ -14,6 +12,21 @@ function paramaters() {
 }
 
 function generate() {
+  if (notrunyet == 1) {
+  notrunyet++
+  element.style.backgroundColor = "gray";
+  var passwordlength = prompt("Please enter how long you want your password to be.");
+  if (passwordlength > 128 || passwordlength < 8) {
+    alert("Password must be at least 8 characters and no more than 128 characters");
+    return;
+  }
+  else {
+  var lowercase = confirm("Do you want to include lowercase letters?")
+  var uppercase = confirm("Do you want to include uppercase letters?")
+  var numbers = confirm("Do you want to include numbers?") //works
+  var specialchar = confirm("Do you want to include special characters?")
+  var containpassword = []
+
   if (lowercase == false) {
     lowerpassword.splice(0, 128,);
   }
@@ -26,7 +39,7 @@ function generate() {
   if (specialchar == false) {
     specialpassword.splice(0, 128,);
   }
-  var newpassword = lowerpassword.concat(upperpassword, numberpassword, specialpassword);
+  var newpassword = containpassword.concat(lowerpassword, upperpassword, numberpassword, specialpassword);
   for (let i = newpassword.length -1; i > 0; i--) {
     let j = Math.floor(Math.random() * i)
     let k = newpassword[i]
@@ -50,6 +63,12 @@ function generate() {
   }
 
   console.log(newpassword);
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = newpassword.join("");
+  console.log("here " + notrunyet)
+}
+}
 }
 
 
