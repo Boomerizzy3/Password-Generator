b597ad9640a9893eff09ec3a2afaff85c69502e5
Tim Mardis <mardistj96@yahoo.com>
2022-08-27 23:10:25 -0500

    created function to generate random password

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,41 @@
-var password = ["a", "B", 1, "%", "a", "B", "c", "D", "e", "F", "g", "H", "i", "J", "k", "L", "m", "N", "o", "P", "q", "R", "s", "T", "u", "V", "w", "X", "y", "Z", "A", "b", "C", "d", "E", "f", "G", "h", "I", "j", "K", "l", "M", "n", "O", "p", "Q", "r", "S", "t", "U", "v", "W", "x", "Y", "z", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "!", "@",];
+var lowerpassword = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f", "g", "h", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "a", "b", "c", "d", "e", "f"];
+var upperpassword = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", ", Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "A", "B", "C", "D", "E", "F"];
+var numberpassword = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8];
+var specialpassword = [ "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")", "!","$", "%", "&", "*", "^", "(", ")", "!", "@", "#", "$", "%", "&", "*", "^", "(", ")"];
+var password = ["yep"];
 var passwordlength = 10
-var lowercase = false
+var lowercase = true
 var uppercase = true
 var numbers = true
-var sepcialchar = true
-
-console.log(password.length);
+var specialchar = true
 
 function generate() {
-  password.length = passwordlength
-  console.log(password);
-  if (lowercase = false) {
-    newpassword = password.map(element => {
-      return element.toUpperCase();
-    })
+  if (lowercase == false) {
+    lowerpassword.splice(0, 128,);
+    console.log(lowerpassword);
   }
-  for (let i = password.length -1; i > 0; i--) {
+  if (uppercase == false) {
+    upperpassword.splice(0, 128,);
+    console.log(upperpassword);
+  }
+  if (numbers == false) {
+    numberpassword.splice(0, 128,);
+    console.log(numberpassword);
+  }
+  if (specialchar == false) {
+    specialpassword.splice(0, 128,);
+    console.log(specialpassword);
+  }
+  var newpassword = lowerpassword.concat(upperpassword, numberpassword, specialpassword);
+  console.log(newpassword);
+  for (let i = newpassword.length -1; i > 0; i--) {
     let j = Math.floor(Math.random() * i)
-    let k = password[i]
-    password[i] = password[j]
-    password[j] = k
+    let k = newpassword[i]
+    newpassword[i] = newpassword[j]
+    newpassword[j] = k
   }
-  console.log(password);
   console.log(newpassword);
 }
-
-
-
 
 
 
